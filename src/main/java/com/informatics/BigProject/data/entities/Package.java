13819d17d315c88eb33fc2991b7f8dfe7139988e
kety99 <kety_99@abv.bg>
2022-02-08 00:34:44 +0200

    Adding reallations between entities.

--- src/main/java/com/informatics/BigProject/data/entities/Package.java
+++ src/main/java/com/informatics/BigProject/data/entities/Package.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
@@ -14,4 +15,14 @@ import javax.persistence.Table;
 @Entity
 @Table(name="package")
 public class Package extends BaseEntity{
+
+    private boolean paid;
+
+    @ManyToOne
+    private Client client;
+
+    @ManyToOne
+    private Employee employee;
+
+
 }
