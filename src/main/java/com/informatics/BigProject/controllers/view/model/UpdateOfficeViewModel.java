f8593a1e3e357bbe4d0675edfc5a102bc779a487
AimiCa <anidimitrovazm@abv.bg>
2022-02-09 17:11:10 +0200

    ViewModelsUpdated

--- src/main/java/com/informatics/BigProject/controllers/view/model/UpdateOfficeViewModel.java
+++ src/main/java/com/informatics/BigProject/controllers/view/model/UpdateOfficeViewModel.java
@@ -5,24 +5,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.OneToOne;
 import javax.validation.constraints.*;
-import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 public class UpdateOfficeViewModel {
-    @NotBlank
-    @Size(min = 3, max = 25, message="Min 3, Max 25")
-    private String name;
-
-    @NotNull
-    @Max(value = 100, message = "Max 100")
-    private int maxNumberOfEmployees;
-
-    @NotNull
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @Past(message="The date has to be in the past!")
-    private LocalDate foundationDate;
+    @OneToOne
+    private String address;
 }
